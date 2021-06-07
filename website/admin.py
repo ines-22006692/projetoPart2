e45539d0e2402cdb4e1f5a5b690000c6c01f02af
ines-22006692 <a22006692@alunos.ulht.pt>
2021-06-07 18:44:03 +0100

    Feito

--- website/admin.py
+++ website/admin.py
@@ -2,7 +2,8 @@ from django.contrib import admin
 from .models import Contacto, Comentario, Quizz, Pessoa
 
 # Register your models here.
+
+admin.site.register(Quizz)
+admin.site.register(Comentario)
 admin.site.register(Pessoa)
 admin.site.register(Contacto)
-admin.site.register(Comentario)
-admin.site.register(Quizz)
